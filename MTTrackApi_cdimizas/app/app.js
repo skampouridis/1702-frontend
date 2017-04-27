9cf0028f41961e7e161b2398ffa1e4f225b6a13d
xristos dimizas <xristos dimizas>
2017-04-28 00:42:34 +0300

    Api service added | vessel tracking data are available per days and mmsi variables

--- MTTrackApi_cdimizas/app/app.js
+++ MTTrackApi_cdimizas/app/app.js
@@ -3,26 +3,22 @@ var $stateProviderRef = null;
 (function () {
   'use strict';
     angular.module('MTTrackingApi', ['ui.router', 'ngAnimate', 'ngRoute', 'leaflet-directive']);
-    angular.module('MTTrackingApi')
-        .config(
-            function($stateProvider, $urlRouterProvider) {
-                $stateProviderRef = $stateProvider;
-                $urlRouterProvider.otherwise('/');
-                $stateProvider
-                // route for the home page
-                    .state('app', {
-                        url:'/',
-                        views: {
-                            'sidebar': {
-                                templateUrl : 'views/sidebar.html',
-                                controller  : 'LandingPageController'
-                            },
-                            'landingPage': {
-                                templateUrl : 'views/landingPage.html',
-                                controller  : 'LandingPageController'
-                            }
-                        }
-                    });
-            }
-        )
+    angular.module('MTTrackingApi').config(
+        function($stateProvider, $urlRouterProvider, $qProvider) {
+            $qProvider.errorOnUnhandledRejections(false);
+            $stateProviderRef = $stateProvider;
+            $urlRouterProvider.otherwise('/');
+            $stateProvider
+            // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'landingPage': {
+                        templateUrl : 'views/landingPage.html',
+                        controller  : 'LandingPageController'
+                     }
+                }
+            });
+        }
+        );
 })();
--- MTTrackApi_cdimizas/app/controllers/LandingPageController.js
+++ MTTrackApi_cdimizas/app/controllers/LandingPageController.js
@@ -2,14 +2,24 @@
 (function () {
     'use strict';
     angular.module('MTTrackingApi').controller('LandingPageController', LandingPageController);
-    LandingPageController.$inject = ['$scope', 'ApiCallService', 'LeafletService', '$timeout'];
+    LandingPageController.$inject = ['$scope', 'ApiCallService', 'LeafletService'];
     // HOME PAGE
-    function LandingPageController($scope, ApiCallService, LeafletService, $timeout) {
+    function LandingPageController($scope, ApiCallService, LeafletService) {
 
-        /*****************
-         * ACTION BUTTONS (Side bar)
-         ****************/
-        $scope.selectDaysModel = 1;
+
+        /*************************
+         * SCOPE VARIABLES
+         *************************/
+
+        // models
+        $scope.selectDaysModel = null;
+        $scope.selectMMSIModel = 219291000;
+        $scope.vesselTrack = {
+            list: []
+        };
+        $scope.loadingVesselTrackingData = false;
+
+        // Side bar elements
         $scope.selectDays = {
             list:[
                 {
@@ -35,13 +45,22 @@
 
             ],
             change: function () {
-                console.log($scope.selectDaysModel);
+                $scope.updateVesselTrack($scope.selectDaysModel, $scope.selectMMSIModel);
+            }
+        };
+        $scope.selectMMSI = {
+            list:[
+                {
+                    id:219291000,
+                    mmsi: "219291000"
+                }
+            ],
+            change: function () {
+                $scope.updateVesselTrack($scope.selectDaysModel, $scope.selectMMSIModel);
             }
         };
 
-        /*************************
-        * Leaflet Map
-        *************************/
+        // Leaflet Map
         $scope.leafletObj = {
             athens:{
                 lat:37.9781853,
@@ -49,6 +68,33 @@
                 zoom: 12
             }
         };
+
+
+
+        /*************************
+         * SCOPE FUNCTIONS
+         *************************/
+        $scope.updateVesselTrack = function (days, mmsi) {
+            $scope.loadingVesselTrackingData = true;
+            ApiCallService.getVesselTrack(days, mmsi).then(
+                // Success
+                function (response) {
+                    $scope.loadingVesselTrackingData = false;
+                    $scope.vesselTrack.list = response;
+                },
+                // error
+                function (error) {
+                    $scope.loadingVesselTrackingData = false;
+                    console.log("No Vessel Tracking Data was found", error);
+                }
+            );
+        };
+
+
+        /*************************
+         * GENERAL FUNCTIONS
+         *************************/
+
 
     }
 })();
