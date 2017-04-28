49201ed2e322f2c7510a7d2c0beef05c2a7ce247
xristos dimizas <xristos dimizas>
2017-04-28 19:55:01 +0300

    Markers are added to map by api call(no animation yet) | probable needs marker grouping in order to avoid overlaps and overloads

--- MTTrackApi_cdimizas/README.md
+++ MTTrackApi_cdimizas/README.md
@@ -1,11 +1,24 @@
 # 1702-frontend
 
 Your task is to present daily vessel tracks on a map and animate them.
-Retrieve dataset from our API URL: http://services.marinetraffic.com/api/exportvesseltrack/3ff05956b0fd72b18a40e69b96c9f4423163490f adding necessary extra parameters like MMSI and days.
+
+Retrieve dataset from our API URL: http://services.marinetraffic.com/api/exportvesseltrack/3ff05956b0fd72b18a40e69b96c9f4423163490f
+adding necessary extra parameters like MMSI and days.
+
 Documentation on the API can be found here: https://www.marinetraffic.com/en/ais-api-services/documentation/api-service:ps01
-The track should be presented both as a line and as waypoints, which present a tooltip with information like vessel speed, course and timestamp.
-Vary the waypoints rendered by zoom level so that the visible waypoints do not overlap. Animation should take vessel speed, true heading and starting time variation in account.
-The user must be able to control animation speed as well as select a point in time. Develop the application on Javascript. Utilizing suitable frameworks is advisable.
+
+The track should be presented both as
+a line and
+as waypoints, which present a tooltip with information like
+   - vessel speed,
+   - course and
+   - timestamp.
+Vary the waypoints rendered by zoom level so that the visible waypoints do not overlap.
+Animation should take vessel speed, true heading and starting time variation in account.
+The user must be able to control animation speed as well as select a point in time. Develop the application on
+Javascript.
+
+Utilizing suitable frameworks is advisable.
 
 Stage your solution on a demo page, fork this repo and create a pull request that contains your implementation in a subfolder.
 
--- MTTrackApi_cdimizas/app/controllers/LandingPageController.js
+++ MTTrackApi_cdimizas/app/controllers/LandingPageController.js
@@ -15,6 +15,7 @@
         $scope.selectDaysModel = null;
         $scope.selectMMSIModel = 219291000;
         $scope.vesselTrack = {
+            shipId: null,
             list: []
         };
         $scope.loadingVesselTrackingData = false;
@@ -27,20 +28,20 @@
                     name: "1 Day"
                 },
                 {
-                    id:2,
-                    name: "2 Days"
-                },
-                {
-                    id:3,
-                    name: "3 Days"
-                },
-                {
-                    id:4,
-                    name: "4 Days"
-                },
-                {
                     id:5,
                     name: "5 Days"
+                },
+                {
+                    id:10,
+                    name: "10 Days"
+                },
+                {
+                    id:20,
+                    name: "20 Days"
+                },
+                {
+                    id:40,
+                    name: "40 Days"
                 }
 
             ],
@@ -62,11 +63,12 @@
 
         // Leaflet Map
         $scope.leafletObj = {
-            athens:{
+            center:{
                 lat:37.9781853,
                 lng:23.7312262,
                 zoom: 12
-            }
+            },
+            markers : {}
         };
 
 
@@ -79,15 +81,25 @@
             ApiCallService.getVesselTrack(days, mmsi).then(
                 // Success
                 function (response) {
-                    $scope.loadingVesselTrackingData = false;
-                    $scope.vesselTrack.list = response;
+                    if(response){
+                        $scope.loadingVesselTrackingData = false;
+                        $scope.vesselTrack.list = response;
+                        $scope.vesselTrack.shipId = response[0].SHIP_ID;
+                    }
                 },
                 // error
                 function (error) {
                     $scope.loadingVesselTrackingData = false;
+                    $scope.vesselTrack.shipId = null;
                     console.log("No Vessel Tracking Data was found", error);
                 }
             );
+        };
+
+        $scope.addMarkersTMap = function () {
+            // TODO - Find Center (on animation the center will be the next marker)
+            $scope.leafletObj.center = setMapCenter($scope.vesselTrack.list[0]);
+            $scope.leafletObj.markers = createMapMarkersList($scope.vesselTrack.list);
         };
 
 
@@ -95,6 +107,34 @@
          * GENERAL FUNCTIONS
          *************************/
 
+        // prepare marker object and add it to directive attribute
+        function prepareMarketObl(currentLocationData){
+            var tooltip = "Speed: "+currentLocationData.SPEED+
+                " Course: " + currentLocationData.COURSE +
+                " At Time: " + new Date(currentLocationData.TIMESTAMP);
+            return {
+                lat: Number(currentLocationData.LAT),
+                lng: Number(currentLocationData.LON),
+                message: tooltip
+            };
+        }
 
+        function setMapCenter(currentLocationData){
+            return {
+                lat:Number(currentLocationData.LAT),
+                lng:Number(currentLocationData.LON),
+                zoom: 3
+            }
+        }
+
+        function createMapMarkersList(markers){
+            var mapObjMarkers = [];
+            for(var i=0; i<markers.length; i++){
+                var markerName = "m"+(i+1);
+                mapObjMarkers[markerName] = prepareMarketObl(markers[i]);
+                // mapObjMarkers.push(prepareMarketObl(markers[i]));
+            }
+            return mapObjMarkers;
+        }
     }
 })();
